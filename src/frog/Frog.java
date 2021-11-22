f2d0f785e6dc3f848851542bdf2e5c391688d51b
Ariane <arainezhang2002@gmail.com>
2021-11-22 16:58:43 +0100

    Class Frog

--- src/frog/Frog.java
+++ src/frog/Frog.java
@@ -8,24 +8,39 @@ import util.Direction;
 public class Frog implements IFrog {
 	
 	private Game game;
+	private Case position;
+	private Direction direction;
 
-	public Frog(Game game)
-	{
+	public Frog(Game game) {
 		this.game = game;
+		position = new Case(game.width/2,0 );
+		direction = Direction.up;
 	}
 
 	@Override
 	public Case getPosition() {
-		return null;
+		return position;
 	}
 
 	@Override
 	public Direction getDirection() {
-		return null;
+		return direction;
 	}
 
 	@Override
 	public void move(Direction key) {
+		if(key == Direction.up && position.ord < game.height-1){
+			position = new Case(position.absc, position.ord+1);
+		}
+		else if(key == Direction.down && position.ord > 0){
+			position = new Case(position.absc, position.ord-1);
+		}
+		else if(key == Direction.left && position.ord > 0 ){
+			position = new Case(position.absc-1, position.ord);
+		}
+		else if(key == Direction.right && position.ord < game.width-1){
+			position = new Case(position.absc+1, position.ord);
+		}
 
 	}
 }
